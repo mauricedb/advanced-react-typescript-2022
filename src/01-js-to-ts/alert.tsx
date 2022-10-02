badb4c646480fb38224d125c46a2405a11df2135
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-10-02 14:37:02 +0200

    React Components using TypeScript

--- src/01-js-to-ts/alert.tsx
+++ src/01-js-to-ts/alert.tsx
@@ -1,14 +1,21 @@
+import { FC } from 'react';
 import { useIntl } from 'react-intl';
 
-export function Alert({ messageId, variant }) {
+type Props = {
+  messageId: string;
+  variant: 'primary' | 'secondary' | 'success' | 'danger';
+};
+
+export const Alert: FC<Props> = ({ messageId, variant }) => {
   const { formatMessage } = useIntl();
 
   if (!messageId) {
     throw new Error('The messageId prop is required');
   }
+
   return (
     <div className={`alert alert-${variant}`} role="alert">
       {formatMessage({ id: messageId })}
     </div>
   );
-}
+};
