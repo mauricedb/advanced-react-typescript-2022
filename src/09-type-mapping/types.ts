28b43883bacc26fea2b48adb9b58b873f6cfe5de
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-10-05 20:04:36 +0200

    Type mapping with Omit<> and/or Pick<>

--- src/09-type-mapping/types.ts
+++ src/09-type-mapping/types.ts
@@ -9,8 +9,6 @@ export type Pizza = z.infer<typeof pizzaSchema>;
 export type ExtraIngredients = z.infer<typeof extraIngredientsSchema>;
 export type ExtraIngredient = z.infer<typeof extraIngredientSchema>;
 
-export type PizzaOnOrder = {
-  name: string;
-  price: number;
+export type PizzaOnOrder = Pick<Pizza, 'name' | 'price'> & {
   extraIngredients: ExtraIngredient[];
 };
