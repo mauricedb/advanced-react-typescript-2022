6f4152ae6bf5b7190a96137473008a58a82c4c71
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-10-05 19:52:41 +0200

    Deriving component prop types

--- src/components/labeled-checkbox.tsx
+++ src/components/labeled-checkbox.tsx
@@ -1,20 +1,16 @@
-import { ChangeEventHandler, FC, ReactNode, useId } from 'react';
+import { ComponentProps, FC, useId } from 'react';
 import { Checkbox } from './checkbox';
 import { Label } from './label';
 
-type Props = {
-  checked: boolean;
-  children: ReactNode;
-  onChange?: ChangeEventHandler;
-};
+type Props = ComponentProps<typeof Checkbox>;
 
-export const LabeledCheckbox: FC<Props> = ({ checked, children, onChange }) => {
+export const LabeledCheckbox: FC<Props> = ({ children, id, ...props }) => {
   const internalId = useId();
 
   return (
     <div className="form-check">
-      <Checkbox checked={checked} id={internalId} onChange={onChange} />
-      <Label htmlFor={internalId}>{children}</Label>
+      <Checkbox id={id ?? internalId} {...props} />
+      <Label htmlFor={id ?? internalId}>{children}</Label>
     </div>
   );
 };
