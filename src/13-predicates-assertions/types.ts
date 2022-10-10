ab264bf3ba8121ecd2002f5cce635752b3c16e1c
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-10-10 21:14:25 +0200

    Type Predicate Functions

--- src/13-predicates-assertions/types.ts
+++ src/13-predicates-assertions/types.ts
@@ -15,3 +15,15 @@ export type Pen = {
 };
 
 export type ItemsOnSale = Book | Magazine | Pen;
+
+export function isBook(item: ItemsOnSale): item is Book {
+  return item.type === 'book';
+}
+
+export function isMagazine(item: ItemsOnSale): item is Magazine {
+  return item.type === 'magazine';
+}
+
+export function isPen(item: ItemsOnSale): item is Pen {
+  return item.type === 'pen';
+}
