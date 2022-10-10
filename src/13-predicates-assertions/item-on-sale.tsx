2555ade6d090f4d4c7d70477ab653966f9358c27
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-10-10 21:31:25 +0200

    Type Assertion Functions

--- src/13-predicates-assertions/item-on-sale.tsx
+++ src/13-predicates-assertions/item-on-sale.tsx
@@ -2,20 +2,23 @@ import type { FC } from 'react';
 import { BookOnSale } from './book-on-sale';
 import { MagazineOnSale } from './magazine-on-sale';
 import { PenOnSale } from './pen-on-sale';
-import { isBook, isMagazine, ItemsOnSale } from './types';
+import { assertBook, assertMagazine, ItemsOnSale } from './types';
 
 type Props = {
   item: ItemsOnSale;
 };
 
 export const ItemOnSale: FC<Props> = ({ item }) => {
-  if (isBook(item)) {
-    return <BookOnSale book={item} />;
-  } else if (isMagazine(item)) {
-    return <MagazineOnSale magazine={item} />;
+  switch (item.type) {
+    case 'book':
+      assertBook(item);
+      return <BookOnSale book={item} />;
+    case 'magazine':
+      assertMagazine(item);
+      return <MagazineOnSale magazine={item} />;
+    // case 'pen':
+    //   return <PenOnSale pen={item as Pen} />;
   }
-  // case 'pen':
-  //   return <PenOnSale pen={item as Pen} />;
 
   return null;
 };
